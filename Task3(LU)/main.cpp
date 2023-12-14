6166a2b0a02f2d14add77703e7361122c575a865
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2023-12-14 21:56:21 +0900

    Update main.cpp

--- Task3(LU)/main.cpp
+++ Task3(LU)/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const int n = 6;  // Размерность матрицы и вектора
+const int n = 5;  // Размерность матрицы и вектора
 
 // Функция для вывода матрицы
 void printMatrix(double mat[][n]) {
@@ -19,6 +19,7 @@ void luDecomposition(double mat[][n], double L[][n], double U[][n]) {
     for (int i = 0; i < n; i++) {
         for (int k = i; k < n; k++) {
             double sum = 0;
+            // Рассчитываем элемент матрицы U
             for (int j = 0; j < i; j++)
                 sum += (L[i][j] * U[j][k]);
             U[i][k] = mat[i][k] - sum;
@@ -26,9 +27,10 @@ void luDecomposition(double mat[][n], double L[][n], double U[][n]) {
 
         for (int k = i; k < n; k++) {
             if (i == k)
-                L[i][i] = 1; // Установка диагональных элементов L равными 1
+                L[i][i] = 1; // Устанавливаем диагональные элементы L равными 1
             else {
                 double sum = 0;
+                // Рассчитываем элемент матрицы L
                 for (int j = 0; j < i; j++)
                     sum += (L[k][j] * U[j][i]);
                 L[k][i] = (mat[k][i] - sum) / U[i][i];
@@ -44,6 +46,7 @@ void solveSystem(double L[][n], double U[][n], double B[], double X[]) {
     // Решение LY = B
     for (int i = 0; i < n; i++) {
         double sum = 0;
+        // Рассчитываем элементы вектора Y
         for (int j = 0; j < i; j++)
             sum += L[i][j] * Y[j];
         Y[i] = B[i] - sum;
@@ -52,6 +55,7 @@ void solveSystem(double L[][n], double U[][n], double B[], double X[]) {
     // Решение UX = Y
     for (int i = n - 1; i >= 0; i--) {
         double sum = 0;
+        // Рассчитываем элементы вектора X
         for (int j = i + 1; j < n; j++)
             sum += U[i][j] * X[j];
         X[i] = (Y[i] - sum) / U[i][i];
@@ -59,15 +63,15 @@ void solveSystem(double L[][n], double U[][n], double B[], double X[]) {
 }
 
 int main() {
-    double A[n][n] = { {9, 1, 5, 7, 6, 28},
-                      {5, 2, 4, 9, 9, 29},
-                      {1, 5, 5, 3, 7, 21},
-                      {1, 7, 8, 2, 6, 24},
-                      {3, 1, 2, 1, 2, 9} };
+    double A[n][n] = { {9, 1, 5, 7, 6},
+                      {5, 2, 4, 9, 9},
+                      {1, 5, 5, 3, 7},
+                      {1, 7, 8, 2, 6},
+                      {3, 1, 2, 1, 2} };
 
     double L[n][n] = { 0 };  // Матрица L
     double U[n][n] = { 0 };  // Матрица U
-    double B[n] = { 28, 29, 21, 24, 9 };  // Вектор свободных членов
+    double B[n] = { 28, 29, 21, 24, 9 };  // Вектор свободных членов (исправленный)
     double X[n] = { 0 };  // Вектор решения
 
     luDecomposition(A, L, U);  // Выполнение LU-разложения
@@ -84,5 +88,4 @@ int main() {
     for (int i = 0; i < n; i++)
         cout << X[i] << " ";
 
-    return 0;
 }
