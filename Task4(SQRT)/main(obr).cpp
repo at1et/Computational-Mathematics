5a27a3697a9a9f7867863a056ce194fa8483540e
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2023-12-14 22:07:01 +0900

    Update main(obr).cpp

--- Task4(SQRT)/main(obr).cpp
+++ Task4(SQRT)/main(obr).cpp
@@ -1,106 +1,111 @@
 #include <iostream>
-#include <cmath>
+#include <iomanip>
+#include <vector>
 
 using namespace std;
 
-const int n = 5;  // Размерность матрицы и вектора
+// Показать матрицу
+void ShowMatrix(vector<vector<double>>& Matrix) {
+    for (int i = 0; i < Matrix.size(); i++) {
+        for (int j = 0; j < Matrix[0].size(); j++) {
+            cout << fixed << Matrix[i][j] << "      ";  // Вывод элемента матрицы с фиксированным числом знаков после запятой
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
-int main() {
-    // Заданные матрица A и вектор B
-    double A[n][n] = {{9, 7, 3, 6, 1},
-                      {7, 9, 3, 7, 3},
-                      {3, 3, 8, 4, 2},
-                      {6, 7, 4, 9, 3},
-                      {1, 3, 2, 3, 2}};
-
-    double B[n] = {45, 51, 43, 53, 21};
-
-    // Инициализация матриц L и U
-    double L[n][n] = {0};
-    double U[n][n] = {0};
-
-    // === Этап №1 ===
-
-    // Заполнение матрицы L
-    L[0][0] = sqrt(A[0][0]);
-
-    for (int i = 1; i < n; i++)
-        L[i][0] = A[i][0] / L[0][0];
+// LU-разложение матрицы
+void Matrix_L(vector<vector<double>>& Matrix, vector<vector<double>>& L) {
+    int n = Matrix.size();
+    L[0][0] = pow(Matrix[0][0], 0.5);  // Инициализация первого элемента матрицы L
+    for (int i = 1; i < n; i++) {
+        L[i][0] = Matrix[i][0] / L[0][0];  // Вычисление элементов первого столбца матрицы L
+    }
 
     for (int k = 1; k < n; k++) {
-        double sqSum = 0;
-        for (int m = 0; m < k; m++)
-            sqSum += L[k][m] * L[k][m];
-        L[k][k] = sqrt(A[k][k] - sqSum);
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum = sum + pow(L[k][i], 2);  // Вычисление суммы квадратов элементов
+        }
+        L[k][k] = pow(Matrix[k][k] - sum, 0.5);  // Вычисление диагональных элементов матрицы L
 
-        if (k < n - 1) {
-            for (int i = k + 1; i < n; i++) {
-                double pairSum = 0;
-                for (int m = 0; m < k; m++)
-                    pairSum += L[i][m] * L[k][m];
-                L[i][k] = (A[i][k] - pairSum) / L[k][k];
+        for (int i = k + 1; i < n; i++) {
+            double sum2 = 0;
+            for (int j = 0; j < k; j++) {
+                sum2 = sum2 + L[i][j] * L[k][j];  // Вычисление элементов ниже диагонали матрицы L
+            }
+            L[i][k] = (Matrix[i][k] - sum2) / L[k][k];  // Заполнение элементов ниже диагонали матрицы L
+        }
+    }
+}
+
+// Получение матрицы Y
+void Matrix_Y(vector<vector<double>>& L, vector<vector<double>>& Y) {
+    int n = L.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j > i) {
+                Y[i][j] = 0;  // Элементы выше главной диагонали матрицы Y
+            }
+            else if (j == i) {
+                Y[i][j] = 1 / L[i][j];  // Диагональные элементы матрицы Y
+            }
+            else {
+                double sum = 0;
+                for (int m = j; m < i; m++) {
+                    sum = sum + L[i][m] * Y[m][j];  // Элементы ниже главной диагонали матрицы Y
+                }
+                Y[i][j] = (-1) / L[i][i] * sum;  // Заполнение элементов ниже главной диагонали матрицы Y
             }
         }
     }
+}
 
-    // Заполнение матрицы U
+// Получение матрицы A
+void Matrix_A(vector<vector<double>>& Y, vector<vector<double>>& A) {
+    int n = Y.size();
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            U[i][j] = L[j][i];
+            A[i][j] = 0;
+            for (int m = 0; m < n; m++) {
+                A[i][j] = A[i][j] + Y[m][i] * Y[m][j];  // Получение матрицы A
+            }
         }
     }
+}
+
+int main() {
+    // Исходная матрица
+    vector<vector<double>> Matrix {{7, 2, 3, 1}, {2, 9, 4, 1}, {3, 4, 8, 5}, {1, 1, 5, 7}};
+    
+    // Матрицы L, Y, A
+    vector<vector<double>> L; vector<vector<double>> Y; vector<vector<double>> A;
+    L.resize(Matrix.size()); Y.resize(Matrix.size()); A.resize(Matrix.size());
+    for (int i = 0; i < L.size(); i++) { L[i].resize(Matrix[i].size()); }
+    for (int i = 0; i < Y.size(); i++) { Y[i].resize(Matrix[i].size()); }
+    for (int i = 0; i < A.size(); i++) { A[i].resize(Matrix[i].size()); }
+
+    // Вывод исходной матрицы
+    ShowMatrix(Matrix);
+
+    // LU-разложение матрицы
+    Matrix_L(Matrix, L);
 
     // Вывод матрицы L
-    cout << "Matrix L:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << L[i][j] << " ";
-        }
-        cout << endl;
-    }
+    ShowMatrix(L);
 
-    // Вывод матрицы U
-    cout << "Matrix U:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << U[i][j] << " ";
-        }
-        cout << endl;
-    }
+    // Получение матрицы Y
+    Matrix_Y(L, Y);
 
-    // === Этап №2 ===
+    // Вывод матрицы Y
+    ShowMatrix(Y);
 
-    // Решение Uy = B
-    double Y[n];
-    for (int i = 0; i < n; i++) {
-        Y[i] = B[i];
-        for (int m = 0; m < i; m++)
-            Y[i] -= U[i][m] * Y[m];
-        Y[i] /= U[i][i];
-    }
+    // Получение матрицы A
+    Matrix_A(Y, A);
 
-    // Вывод вектора Y
-    cout << "Vector Y:\n";
-    for (int i = 0; i < n; i++)
-        cout << Y[i] << " ";
-    cout << endl;
-
-    // === Этап №3 ===
-
-    // Решение Lx = y
-    double X[n];
-    for (int i = n - 1; i >= 0; i--) {
-        X[i] = Y[i];
-        for (int m = i + 1; m < n; m++)
-            X[i] -= L[i][m] * X[m];
-        X[i] /= L[i][i];
-    }
-
-    // Вывод вектора X
-    cout << "Solution X:\n";
-    for (int i = 0; i < n; i++)
-        cout << X[i] << " ";
-    cout << endl;
+    // Вывод матрицы A
+    ShowMatrix(A);
 
     return 0;
 }
